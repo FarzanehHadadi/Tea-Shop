ae39708e54d5b40de2b40ef8842df91db5b43739
farzanehHaddadi <farzhadadi@gmail.com>
2021-07-06 22:34:34 +0430

    nav bar completed

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
+    
+
+    // navBtn
+    $('.navBtn').click(function(){$('.nav-items').slideToggle(1000)})
+    // magnifier
   $('.image-link').magnificPopup({type:'image'});
+  //owl
   $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
@@ -22,3 +28,25 @@ $(document).ready(function() {
 })
 
 });
+// nav-fix
+$(window).scroll(function(){
+    let scroll = $(window).scrollTop();
+    console.log(scroll);
+    if(scroll > 70){
+        console.log($('nav'));
+        $('nav').addClass('nav-fixed');
+    }
+    else{
+        $('nav').removeClass('nav-fixed');
+
+    }
+})
+
+$('.nav-items a').click(function(link){
+
+    link.preventDefault();
+    let target = $(this).attr('href');
+    $('html, body').animate({
+        scrollTop: $(target).offset().top
+    },'slow')
+})
